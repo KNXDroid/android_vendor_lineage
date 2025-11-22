a4379079c967d063c4540651fe4645287433282c
danya2271 <dany2271@yandex.ru>
2025-11-22 15:05:56 +0500

    config: common: Build with GAPPS by default
    Change-Id: I82598d3eafc5e54dee3d19dad2de154a146605af

--- config/common.mk
+++ config/common.mk
@@ -70,6 +70,13 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.ota.allow_downgrade=true
 endif
 
+# Gapps
+WITH_GAPPS ?= true
+
+ifeq ($(WITH_GAPPS),true)
+  $(call inherit-product,  vendor/gapps/arm64/arm64-vendor.mk )
+endif
+
 # Lineage-specific broadcast actions whitelist
 PRODUCT_COPY_FILES += \
     vendor/lineage/config/permissions/lineage-sysconfig.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/lineage-sysconfig.xml
